1db82f5668c21fcc7b3d1515538659fd37650198
Cicak Bin Kadal <70104051+cbkadal@users.noreply.github.com>
2021-09-28 17:28:01 +0700

    modified:   04-pointers-of-pointers-of-pointers.c

--- Demos/Week04/04-pointers-of-pointers-of-pointers.c
+++ Demos/Week04/04-pointers-of-pointers-of-pointers.c
@@ -5,6 +5,7 @@
  * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # INFO: TBA
+ * REV07: Tue 28 Sep 2021 17:16:52 WIB
  * REV06: Sun Feb 23 16:59:31 WIB 2020
  * REV05: Tue Oct  1 11:37:10 WIB 2019
  * REV04: Tue Aug 28 18:40:36 WIB 2018
@@ -33,11 +34,19 @@ void main(void) {
 }
 
 /*
- * TAKE NOTE (MIK)
- * Line 26 : ppptr0 akan menyimpan alamat dari ptrptr0
- * Line 42 : ppptr0 akan berisi alamat dari ptrptr0,
- * &ppptr0 akan berisi alamat dari ppptr0, dan
- * *ppptr0 akan berisi isi dari
- * ptrptr0 yang mana adalah alamat dari ptrchr0.
- */
+  VARIABLE  +++ ADDRESS ++ + VALUE(CH) ++
+  varchr0 = 0x556677881038    0X61 (a)
+  ptrchr0 = 0x556677881040 0x556677881038
+  ptrptr0 = 0x556677881048 0x556677881040
+   ppptr0 = 0x556677881050 0x556677881048
+  *ppptr0 = 0x556677881048 0x556677881040
+ **ppptr0 = 0x556677881040 0x556677881038
+***ppptr0 = 0x556677881038    0X61 (a)
+
+                      0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
+0000 5566 7788 1030   -  -  -  -  -  -  -  - 61  -  -  -  -  -  -  -
+0000 5566 7788 1040  38 10 88 77 66 55 00 00 40 10 88 77 66 55 00 00 
+0000 5566 7788 1050  48 10 88 77 66 55 00 00  -  -  -  -  -  -  -  -
+*/
+
 
