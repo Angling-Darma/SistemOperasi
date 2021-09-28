182474ef371277e9882a65446f228c5648fa64bc
Cicak Bin Kadal <70104051+cbkadal@users.noreply.github.com>
2021-09-28 23:43:56 +0700

    modified:   Demos/Week04/04-pointers-of-pointers-of-pointers.c

--- Demos/Week04/04-pointers-of-pointers-of-pointers.c
+++ Demos/Week04/04-pointers-of-pointers-of-pointers.c
@@ -14,7 +14,6 @@
  */
 
 #include <stdio.h>
-#define HEAD1 "  VARIABLE  +++ ADDRESS ++ + VALUE(CH) ++\n"
 
 /* Global Variables */
 unsigned char   varchr0='a';
@@ -23,30 +22,43 @@ unsigned char** ptrptr0=&ptrchr0;
 unsigned char*** ppptr0=&ptrptr0;
 
 void main(void) {
-   printf(HEAD1);
-   printf("  varchr0 = %14p %#7X (%c)\n", &varchr0,   varchr0, varchr0);
-   printf("  ptrchr0 = %14p %14p\n",      &ptrchr0,   ptrchr0);
-   printf("  ptrptr0 = %14p %14p\n",      &ptrptr0,   ptrptr0);
-   printf("   ppptr0 = %14p %14p\n",       &ppptr0,   ppptr0);
-   printf("  *ppptr0 = %14p %14p\n",        ppptr0,  *ppptr0);
-   printf(" **ppptr0 = %14p %14p\n",       *ppptr0, **ppptr0);
-   printf("***ppptr0 = %14p %#7X (%c)\n", **ppptr0,***ppptr0,***ppptr0);
+   printf("Address  &ppptr0 = %#16.16lX\n",   &ppptr0);
+   printf("Value     ppptr0 = %#16.16lX\n",    ppptr0);
+   printf("Address &ptrptr0 = %#16.16lX\n",  &ptrptr0);
+   printf("Pointer  *ppptr0 = %#16.16lX\n",   *ppptr0);
+   printf("Value    ptrptr0 = %#16.16lX\n",   ptrptr0);
+   printf("Address &ptrchr0 = %#16.16lX\n",  &ptrchr0);
+   printf("PtrPtr  **ppptr0 = %#16.16lX\n",  **ppptr0);
+   printf("Pointer *ptrptr0 = %#16.16lX\n",  *ptrptr0);
+   printf("Value    ptrchr0 = %#16.16lX\n",   ptrchr0);
+   printf("Address &varchr0 = %#16.16lX\n",  &varchr0);
+   printf("PPPtr  ***ppptr0 = %18c\n",      ***ppptr0);
+   printf("PtrPtr **ptrptr0 = %18c\n",      **ptrptr0);
+   printf("Pointer *ptrchr0 = %18c\n",       *ptrchr0);
+   printf("Value    varchr0 = %18c\n\n",      varchr0);
+
 }
 
 /*
-  VARIABLE  +++ ADDRESS ++ + VALUE(CH) ++
-  varchr0 = 0x556677881038    0X61 (a)
-  ptrchr0 = 0x556677881040 0x556677881038
-  ptrptr0 = 0x556677881048 0x556677881040
-   ppptr0 = 0x556677881050 0x556677881048
-  *ppptr0 = 0x556677881048 0x556677881040
- **ppptr0 = 0x556677881040 0x556677881038
-***ppptr0 = 0x556677881038    0X61 (a)
+Address  &ppptr0 = 0X0000556677889948
+Value     ppptr0 = 0X0000556677889940
+Address &ptrptr0 = 0X0000556677889940
+Pointer  *ppptr0 = 0X0000556677889938
+Value    ptrptr0 = 0X0000556677889938
+Address &ptrchr0 = 0X0000556677889938
+PtrPtr  **ppptr0 = 0X0000556677889930
+Pointer *ptrptr0 = 0X0000556677889930
+Value    ptrchr0 = 0X0000556677889930
+Address &varchr0 = 0X0000556677889930
+PPPtr  ***ppptr0 =                  a
+PtrPtr **ptrptr0 =                  a
+Pointer *ptrchr0 =                  a
+Value    varchr0 =                  a
+
 
                       0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
-0000 5566 7788 1030   -  -  -  -  -  -  -  - 61  -  -  -  -  -  -  -
-0000 5566 7788 1040  38 10 88 77 66 55 00 00 40 10 88 77 66 55 00 00 
-0000 5566 7788 1050  48 10 88 77 66 55 00 00  -  -  -  -  -  -  -  -
+0000 5566 7788 9930  61  -  -  -  -  -  -  - 30 99 88 77 66 55 00 00 
+0000 5566 7788 9940  38 99 88 77 66 55 00 00 40 99 88 77 66 55 00 00 
 */
 
 
