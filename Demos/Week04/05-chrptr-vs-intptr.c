e53a4a2b43b1d746b84c0d5af1c793ad1f2235f7
Cicak Bin Kadal <70104051+cbkadal@users.noreply.github.com>
2021-09-29 11:30:52 +0700

    modified:   Demos/Week04/05-chrptr-vs-intptr.c

--- Demos/Week04/05-chrptr-vs-intptr.c
+++ Demos/Week04/05-chrptr-vs-intptr.c
@@ -5,7 +5,7 @@
  * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # INFO: TBA
- * REV07: Wed 29 Sep 2021 08:22:52 WIB
+ * REV07: Wed 29 Sep 2021 11:30:00 WIB
  * REV06: Sun Feb 23 16:59:31 WIB 2020
  * REV05: Tue Aug 28 18:40:36 WIB 2018
  * REV04: Tue Oct  3 21:18:21 WIB 2017
@@ -14,8 +14,6 @@
 
 #include <stdio.h>
 
-#define HEAD2 "!!! ptrint0=(int*) &varchr1;  varint0=*ptrint0; !!!\n"
-#define HEAD4 "!!! ptrchr0=(char*) &varint0; varchr0=*ptrchr0; !!!\n"
 #define HEAD5 "!!!! !!!!  ptrchr0++;  varchr0=*ptrchr0;  !!!! !!!!\n"
 
 /* Global Variables */
@@ -30,15 +28,21 @@ unsigned int*   ptrint0=&varint0;
 unsigned char*  ptrchr0=&varchr0;
 
 void main(void) {
-   printf("Address:     varint0 = %#16.16lX\n",  &varint0);
-   printf("Character:   varint0 = %c\n",  varint0);
-   printf("Decimal:     varint0 = %d\n",  varint0);
-   printf("Hexadecimal: varint0 = %#X\n", varint0);
+   printf("Address:     &ptrint0 = %#16.16lX\n",  &ptrint0);
+   printf("Value:        ptrint0 = %#16.16lX\n",   ptrint0);
+   printf("Address:     &varint0 = %#16.16lX\n",  &varint0);
+   printf("Pointer:     *ptrint0 = %#X\n", *ptrint0);
+   printf("Hexadecimal:  varint0 = %#X\n", varint0);
+   printf("Decimal:      varint0 = %d\n",  varint0);
+   printf("Character:    varint0 = %c\n",  varint0);
    printf("\n");
-   printf("Address:     varchr0 = %#16.16lX\n",  &varchr0);
-   printf("Character:   varchr0 = %c\n",  varchr0);
-   printf("Decimal:     varchr0 = %d\n",  varchr0);
-   printf("Hexadecimal: varchr0 = %#X\n", varchr0);
+   printf("Address:     &ptrchr0 = %#16.16lX\n",  &ptrchr0);
+   printf("Value:        ptrchr0 = %#16.16lX\n",   ptrchr0);
+   printf("Address:     &varchr0 = %#16.16lX\n",  &varchr0);
+   printf("Pointer:     *ptrchr0 = %c\n", *ptrchr0);
+   printf("Character:    varchr0 = %c\n",  varchr0);
+   printf("Decimal:      varchr0 = %d\n",  varchr0);
+   printf("Hexadecimal:  varchr0 = %#X\n", varchr0);
    printf("\n");
    printf("varchr1: Address = %#16.16lX -- Character = %c -- Decimal = %3d -- Hexa = %#X\n",
           &varchr1, varchr1, varchr1, varchr1);
@@ -50,14 +54,26 @@ void main(void) {
           &varchr4, varchr4, varchr4, varchr4);
    printf("\n");
 
-   printf("ptrint0 = %10p  %14p %12X\n", ptrint0, &ptrint0, *ptrint0);
-   printf("ptrchr0 = %10p  %14p %12c\n",  ptrchr0, &ptrchr0, *ptrchr0);
-   printf(HEAD2);
-   ptrint0=(int*) &varchr1;
-   varint0=*ptrint0;
-   printf("ptrint0 = %10p %14p %#12X\n", ptrint0, &ptrint0, *ptrint0);
-   printf("varint0 = %#10X = %c %10p\n", varint0, varint0, &varint0);
-   printf(HEAD4);
+   printf("Value:        ptrint0 = %#16.16lX\n",   ptrint0);
+   printf("Address:     &varchr1 = %#16.16lX\n",  &varchr1);
+   printf("ptrint0=(int*)&varchr1; // =======\n"); ptrint0=(int*) &varchr1;
+   printf("Value:        ptrint0 = %#16.16lX\n",   ptrint0);
+   printf("\n");
+
+   printf("Value:        varint0 = %#X\n", varint0);
+   printf("Pointer:     *ptrint0 = %#X\n", *ptrint0);
+   printf("varint0=*ptrint0; //  = ==========\n"); varint0=*ptrint0;
+   printf("Value:        varint0 = %#X\n", varint0);
+   printf("\n");
+
+// #define HEAD4 "!!! ptrchr0=(char*) &varint0; varchr0=*ptrchr0; !!!\n"
+
+   printf("Value:        ptrchr0 = %#16.16lX\n",   ptrchr0);
+   printf("Address:     &varint0 = %#16.16lX\n",  &varint0);
+   printf("ptrchr0=(char*)&varint0;// =======\n"); ptrchr0=(char*) &varint0;
+   printf("Value:        ptrchr0 = %#16.16lX\n",   ptrchr0);
+   printf("\n");
+
    ptrchr0=(char*) &varint0; 
    varchr0=*ptrchr0;
    printf("ptrchr0 = %10p %14p %#12X\n", ptrchr0, &ptrchr0, *ptrchr0);
@@ -70,15 +86,19 @@ void main(void) {
 }
 
 /*
-Address:    &varint0 = 0X0000556677889940
-Character:   varint0 = D
-Decimal:     varint0 = 1094861636
-Hexadecimal: varint0 = 0X41424344
+Address:     &ptrint0 = 0X0000556677889950
+Value:        ptrint0 = 0X0000556677889940
+Address:     &varint0 = 0X0000556677889940
+Character:    varint0 = D
+Decimal:      varint0 = 1094861636
+Hexadecimal:  varint0 = 0X41424344
 
-Address:    &varchr0 = 0X0000556677889944
-Character:   varchr0 = a
-Decimal:     varchr0 = 97
-Hexadecimal: varchr0 = 0X61
+Address:     &ptrchr0 = 0X0000556677889958
+Address:      ptrchr0 = 0X0000556677889944
+Address:     &varchr0 = 0X0000556677889944
+Character:    varchr0 = a
+Decimal:      varchr0 = 97
+Hexadecimal:  varchr0 = 0X61
 
 varchr1: Address = 0X0000556677889945 -- Character = b -- Decimal =  98 -- Hexa = 0X62
 varchr2: Address = 0X0000556677889946 -- Character = c -- Decimal =  99 -- Hexa = 0X63
@@ -86,6 +106,35 @@ varchr3: Address = 0X0000556677889947 -- Character = d -- Decimal = 100 -- Hexa 
 varchr4: Address = 0X0000556677889948 -- Character = e -- Decimal = 101 -- Hexa = 0X65
 
                       0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
-0000 5566 7788 9940  44 43 32 41 61 62 63 64 65  -  -  -  -  -  -  -
+0000 5566 7788 9940  44 43 42 41 61 62 63 64 65  -  -  -  -  -  -  -
+0000 5566 7788 9950  40 99 88 77 66 55 00 00 44 99 88 77 66 55 00 00
+
+Value:        ptrint0 = 0X0000556677889940
+Address:     &varchr1 = 0X0000556677889945
+ptrint0=(int*)&varchr1; // =======
+Value:        ptrint0 = 0X0000556677889945
+
+                      0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
+0000 5566 7788 9940  44 43 42 41 61 62 63 64 65  -  -  -  -  -  -  -
+0000 5566 7788 9950  45 99 88 77 66 55 00 00 44 99 88 77 66 55 00 00
+
+Value:        varint0 = 0X41424344
+Pointer:     *ptrint0 = 0X65646362
+varint0=*ptrint0; //  = ==========
+Value:        varint0 = 0X65646362
+
+                      0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
+0000 5566 7788 9940  62 63 64 65 61 62 63 64 65  -  -  -  -  -  -  -
+0000 5566 7788 9950  45 99 88 77 66 55 00 00 44 99 88 77 66 55 00 00
+
+Value:        ptrchr0 = 0X000055CF05D83044
+Address:     &varint0 = 0X000055CF05D83040
+ptrchr0=(char*)&varint0;// =======
+Value:        ptrchr0 = 0X000055CF05D83040
+
+                      0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
+0000 5566 7788 9940  62 63 64 65 61 62 63 64 65  -  -  -  -  -  -  -
+0000 5566 7788 9950  45 99 88 77 66 55 00 00 40 99 88 77 66 55 00 00
+
  */
 
